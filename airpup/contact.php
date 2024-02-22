fe1a13cdada5eac4bf4e088e77159ebd41a012cb
gm485 <gm485@tester>
2024-02-22 13:53:49 +0000

    Symphony Tutorial 2

---
+++ airpup/contact.php
@@ -0,0 +1,7 @@
+<?php 
+    require 'lib/functions.php';
+?>
+<?php require 'layout/header.php' ?>
+<h1>Helping you find your new best friend from over <?php echo count(get_pets());?> pets!</h1>
+<?php  echo 'Contact Us!'; ?>
+<?php require 'layout/footer.php'?>
---
+++ airpup/layout/footer.php
@@ -0,0 +1,4 @@
+
+<footer>
+	<p>&copy; AirPupnMeow.com</p>
+</footer>
